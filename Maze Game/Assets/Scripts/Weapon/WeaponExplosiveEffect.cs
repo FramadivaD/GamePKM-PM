084c3a7afd54369a3f2b05c71ff2a3126820858e
Syafiqjos <syafiqjos@gmail.com>
2021-07-23 18:44:52 +0700

    Add weapon projectile effect particle

--- Maze Game/Assets/Scripts/Weapon/WeaponExplosiveEffect.cs
+++ Maze Game/Assets/Scripts/Weapon/WeaponExplosiveEffect.cs
@@ -3,5 +3,10 @@ using System.Collections;
 
 public class WeaponExplosiveEffect : MonoBehaviour
 {
+    [SerializeField] private float destroyAfter = 5;
 
+    private void Start()
+    {
+        Destroy(gameObject, destroyAfter);
+    }
 }
